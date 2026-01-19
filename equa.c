9e0cb98b43ccd945696ac2ea16bb54d2faf7bbbb
自由Kay <venancekayode86@gmail.com>
2026-01-19 13:10:45 +0100

    Update equa.c

--- equa.c
+++ equa.c
@@ -24,27 +24,27 @@ int main(void)
         }
         else if(a == 0){
             x = -c/b;
-            printf("x = %f, soit SR l'ensemble des solutions dans R, SR:{%f}", x ,x);
+            printf("L'equation admet une solution unique x = %.1f, soit SR l'ensemble des solutions dans R, SR:{%.1f}", x ,x);
         }
 
-        delta = pow(b,2)-(4*a*c);
-
+        
         if (a != 0){
-            printf("Δ= %f\n", delta);
+            delta = pow(b,2)-(4*a*c);
+            printf("Δ= %2.f\n", delta);
         }
 
         if (delta > 0 && a != 0){
             x1 = (-b+sqrt(delta))/(2*a);
             x2 = (-b-sqrt(delta))/(2*a);
-            printf("Δ>0 alors l'équation admet deux solutions distinctes X1 et X2 telles que X1= %f et X2= %f\nSoit SR l'ensemble des solutions dans R, SR:{%f;%f}", x1, x2, x1, x2); 
+            printf("Δ>0 alors l'équation admet deux solutions distinctes X1 et X2 telles que X1= %f et X2= %f\nSoit SR l'ensemble des solutions dans R, SR:{%f;%f}\nMerci d'avoir utilisé ce programme.", x1, x2, x1, x2); 
         }
         else if(delta == 0 && a != 0){
             x0 = (-b)/(2*a);
-            printf("Δ= %f, alors l'équation admet une solution unique X0 telle que X0= %f\nSoit SR l'ensemble des solutions dans R, SR:{%f}", delta, x0, x0);
+            printf("Δ= %2.f, alors l'équation admet une solution unique X0 telle que X0= %.2f\nSoit SR l'ensemble des solutions dans R, SR:{%.2f}\nMerci d'avoir utilisé ce programme.", delta, x0, x0);
         }
 
         else if(delta < 0 && a != 0){
-            printf("Δ<0, alors l'équation n'admet aucune solution dans R\nSoit SR l'ensemble des solutions dans R, SR:{ }");
+            printf("Δ<0, alors l'équation n'admet aucune solution dans R\nSoit SR l'ensemble des solutions dans R, SR:{ }\nMerci d'avoir utilisé ce programme.");
         }
     return(0);
 }
